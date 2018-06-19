09f54045eaec174aadf520b635f1516016da8e62
petrikov <fhsaymon@hotmail.com>
2018-06-19 01:05:21 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,28 @@
 
+// caixa de diálogo
 $( function() {
-   $( "#dialog1" ).dialog({
-      autoOpen: false,
-      show: {
-         effect: "shake",
-         duration: 1000
-      },
-      hide: {
-         effect: "explode",
-         duration: 1000
-      }
-   });
+ $( "#dialog1" ).dialog({
+  autoOpen: false,
+  show: {
+   effect: "shake",
+   duration: 1000
+ },
+ hide: {
+   effect: "explode",
+   duration: 1000
+ }
+});
 
-   $( "#opener" ).on( "click", function() {
-      $( "#dialog1" ).dialog( "open" );
-   });
+ $( "#opener" ).on( "click", function() {
+  $( "#dialog1" ).dialog( "open" );
+});
 } );
 
 
 
 //fancybox
 $('[data-fancybox]').fancybox({
-   protect: true
+ protect: true
 });
 $.fancybox.defaults.animationEffect = "zoom-in-out";
 
@@ -29,127 +30,56 @@ $.fancybox.defaults.animationEffect = "zoom-in-out";
 
 //marcador do menu
 $( function() {
-  // Get current path and find target link
-  var path = window.location.pathname.split("/").pop();
   
-  // Account for home page with empty path
+  var path = window.location.pathname.split("/").pop();
   if ( path == '' ) {
     path = 'index.html';
   }
-      
   var target = $('nav a[href="'+path+'"]'); 
-  // Add active class to target link
   target.addClass('active');
 });
 
 
 
-
-// aqui começa o negocio de download
-// $( function() {
-//     var progressTimer,
-//       progressbar = $( "#progressbar" ),
-//       progressLabel = $( ".progress-label" ),
-//       dialogButtons = [{
-//         text: "Cancel Download",
-//         click: closeDownload
-//       }],
-//       dialog = $( "#dialog" ).dialog({
-//         autoOpen: false,
-//         closeOnEscape: false,
-//         resizable: false,
-//         buttons: dialogButtons,
-//         open: function() {
-//           progressTimer = setTimeout( progress, 2000 );
-//         },
-//         beforeClose: function() {
-//           downloadButton.button( "option", {
-//             disabled: false,
-//             label: "Start Download"
-//           });
-//         }
-//       }),
-//       downloadButton = $( "#downloadButton" )
-//         .button()
-//         .on( "click", function() {
-//           $( this ).button( "option", {
-//             disabled: true,
-//             label: "Downloading..."
-//           });
-//           dialog.dialog( "open" );
-//         });
- 
-//     progressbar.progressbar({
-//       value: false,
-//       change: function() {
-//         progressLabel.text( "Current Progress: " + progressbar.progressbar( "value" ) + "%" );
-//       },
-//       complete: function() {
-//         progressLabel.text( "Complete!" );
-//         dialog.dialog( "option", "buttons", [{
-//           text: "Close",
-//           click: closeDownload
-//         }]);
-//         $(".ui-dialog button").last().trigger( "focus" );
-//       }
-//     });
- 
-//     function progress() {
-//       var val = progressbar.progressbar( "value" ) || 0;
- 
-//       progressbar.progressbar( "value", val + Math.floor( Math.random() * 3 ) );
- 
-//       if ( val <= 99 ) {
-//         progressTimer = setTimeout( progress, 50 );
-//       }
-//     }
- 
-//     function closeDownload() {
-//       clearTimeout( progressTimer );
-//       dialog
-//         .dialog( "option", "buttons", dialogButtons )
-//         .dialog( "close" );
-//       progressbar.progressbar( "value", false );
-//       progressLabel
-//         .text( "Starting download..." );
-//       downloadButton.trigger( "focus" );
-//     }
-//   } );
-
 // animacao de scroll
 
 $(function() {
   $('.scroll0').on('click', function(e) {
-   
+
     $('html, body').animate({ scrollTop: $("#hero").offset().top}, 500);
   });
 });
 
 $(function() {
   $('.scroll1').on('click', function(e) {
-   
+
     $('html, body').animate({ scrollTop: $("#about").offset().top}, 500);
   });
 });
 
 $(function() {
   $('.scroll2').on('click', function(e) {
-   
+
     $('html, body').animate({ scrollTop: $("#services").offset().top}, 500);
   });
 });
 
 
+//Codigo do botão.
+
 $(function() {
-   var link = $(".menu li a");
-      var link = $(".btn");
+  var subir = $(".btn");
+  subir.fadeOut();
+  $(window).scroll(function(){
+    var posicao = $(this).scrollTop();
+    if(posicao >= 999){
+      subir.fadeIn();
+    }else{
+      subir.fadeOut();
+    }
+  })
 
-      link.on("click", function(){
-         alert("ainda vo implementar. calma ai.");
-         var seletor = $(this).attr("href");
-         var posicao = $(seletor).offset().top;
-          $('html, body').animate({ scrollTop: posicao })
-
-
-      })
+  subir.on("click", function(){
+    $("html,body").animate({scrollTop: 0}, 500);
+  })
 });
